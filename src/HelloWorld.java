b2fc25337a340ed80e6db03b3575347f2738c6b4
Victor Zinoviev <viczinoviev@gmail.com>
2024-10-19 15:38:57 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public final class HelloWorld {
+
+    private HelloWorld() {
+        // no code needed here
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
